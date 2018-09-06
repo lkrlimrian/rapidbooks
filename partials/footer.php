b5ca019f06717f511490e845be69d74f4becdca9
Yves Garcia <pjydgarcia@gmail.com>
2018-09-06 13:15:49 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -2,9 +2,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
     <script src="assets/js/plugins.js"></script>
     <script src="assets/js/main.js"></script>
+
     <script src="assets/js/donut.js"></script>
     <script src="assets/js/redirect.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="assets/js/lib/chart-js/Chart.bundle.js"></script>
     <script src="assets/js/dashboard.js"></script>
     <script src="assets/js/widgets.js"></script>
